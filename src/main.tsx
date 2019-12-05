8d62e308ebb241e6251a2b98fd4bbd7f3bfaf0a1
Marco Antonio <masjr@amazon.com>
2019-12-05 17:52:31 -0300

    ENH: add redux dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import { render } from 'ink'
+import { Provider } from 'react-redux'
+import { generateStore } from './redux/store'
 
 import App from './App'
 
-render(<App />)
+const store = generateStore()
+
+const ReduxContext = createContext({
+  store: {}
+})
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
