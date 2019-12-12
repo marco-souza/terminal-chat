05e6cc4649dbb66df57f77b7085135263a8eb13b
Marco Antônio <ma.souza.junior@gmail.com>
2019-12-12 00:31:05 -0300

    ENH: remove title

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box } from 'ink'
+import { Box, Text, Color } from 'ink'
 import TextInput from 'ink-text-input';
 
 import useMessages from '../redux/messages';
@@ -18,7 +18,12 @@ export default () => {
 
   return (
     <Box padding={1}>
-      > {
+      {<Color green>
+        <Text key='title' bold underline>
+          {process.env.USER || 'tchat'}
+        </Text>
+      </Color>
+      }: {
         <TextInput
           placeholder='enter your msg here'
           value={message}
--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -7,24 +7,15 @@ export default () => {
   const { messageList } = useMessages()
 
   return (
-    <Box flexGrow={1}>
+    <Box>
       <Static>
-        {[
-          <Box width='100%' key='title' padding={1}>
-            <Color blue>
-              <Text key='title' bold underline>
-                tchat
-            </Text>
-            </Color>
-          </Box>,
-          ...messageList
-            .map(({ username, message }) => (
-              <Box width='100%' key={username + message} paddingLeft={2}>
-                <Text bold>{username} said: </Text>
-                <Text>{message}</Text>
-              </Box>
-            ))
-        ]}
+        {messageList
+          .map(({ username, message }) => (
+            <Box width='100%' key={username + message} paddingLeft={2}>
+              <Text bold>{username} said: </Text>
+              <Text>{message}</Text>
+            </Box>
+          ))}
       </Static>
     </Box>
   )
