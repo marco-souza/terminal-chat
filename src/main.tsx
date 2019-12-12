40667cbd58fffbfd4aa69dfcb882aa837070f06c
Marco Antônio <ma.souza.junior@gmail.com>
2019-12-11 22:33:41 -0300

    ENH: clear terminal before start

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react'
+import React from 'react'
 import { render } from 'ink'
 import { Provider } from 'react-redux'
 import { generateStore } from './redux/store'
@@ -6,10 +6,7 @@ import { generateStore } from './redux/store'
 import App from './App'
 
 const store = generateStore()
-
-const ReduxContext = createContext({
-  store: {}
-})
+console.clear()
 
 render(
   <Provider store={store}>
